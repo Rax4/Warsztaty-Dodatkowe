c83db16807383920c9e975e52e6a71a2d5c14d1e
Piotr Raksimowicz <piotr.rx@gmail.com>
2017-02-06 15:36:50 +0100

    Początki. Gettery Settery i construct

---
+++ class/User.php
@@ -0,0 +1,85 @@
+<?php
+/***
+ * Class represents one user
+ * 
+ */
+class User
+{
+    private $id;
+    private $address;
+    private $name;
+    private $surname;
+    private $credits;
+    private $hashedPassword;
+    
+    public function __construct() 
+    {
+        $this->id = -1;
+        $this->address = '';
+        $this->name = '';
+        $this->surname = '';
+        $this->credits = null;
+        $this->hashedPassword = '';
+    }
+    
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return true;
+    }
+    
+    public function setName($name)
+    {
+        $this->name = $name;
+        return true;
+    }
+    
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+        return true;
+    }
+    
+    public function setCredis($credits)
+    {
+        $this->credits = $credits;
+        return true;
+    }
+    
+    public function setHashedPassword($password)
+    {
+        $newHashedPassword = password_hash($password,PASSWORD_BCRYPT);
+        $this->hashedPassword = $newHashedPassword;
+        return true;
+    }
+    
+    public function getAddress()
+    {
+        return $this->address;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+    
+    public function getCredis()
+    {
+        return $this->credits;
+    }
+    
+    public function getHashedPassword()
+    {
+        return $this->hashedPassword;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+}
